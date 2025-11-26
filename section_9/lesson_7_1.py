e40bd6ddd8e17f5e53a90842e396a5637403583d
Ahmed I <ahmedirezk9@gmail.com>
2025-11-27 00:38:38 +0200

    Add script to count characters with and without spaces

---
+++ section_9/lesson_7_1.py
@@ -0,0 +1,8 @@
+text = """A computer is a digital electronic machine that 
+can be programmed to carry out sequences of arithmetic or 
+logical operations automatically"""
+
+new_text = text.replace(" ","")
+
+print("Namber of characters with spaces: ", len(text))
+print("Namber of characters without spaces: ", len(new_text))
